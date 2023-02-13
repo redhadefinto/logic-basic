54311ed889a25366183142157ca0a0dc175d564c
redhadefinto <redhadefinto28@gmail.com>
2023-02-13 22:02:18 +0700

    logic input.length > 2

--- PalindromeWeb/Palindrome.js
+++ PalindromeWeb/Palindrome.js
@@ -5,19 +5,23 @@ const palindrome = () => {
   btn.addEventListener('click', () => {
     let pattern = /^[A-Z,a-z]+$/;
     if(input.value.match(pattern)) {
-      const result = input.value.toLowerCase();
-      console.log(input.value)
-      let palindrome = [...result];
-      palindrome = palindrome.reverse();
-      palindrome = palindrome.join("");
-      palindrome = palindrome.toString();
-      console.log(palindrome)
-      // console.log(`input Awal: ${input}`);
-      // console.log(`input palindrome: ${palindrome}`);
-      if (palindrome == result) {
-        return containerResult.innerHTML = `<p>Kata yang anda masukan <b>palindrome</b></p>`
+      if(input.value.length > 2) {
+        const result = input.value.toLowerCase();
+        console.log(input.value)
+        let palindrome = [...result];
+        palindrome = palindrome.reverse();
+        palindrome = palindrome.join("");
+        palindrome = palindrome.toString();
+        console.log(palindrome)
+        // console.log(`input Awal: ${input}`);
+        // console.log(`input palindrome: ${palindrome}`);
+        if (palindrome == result) {
+          return containerResult.innerHTML = `<p>Kata yang anda masukan <b>palindrome</b></p>`
+        } else {
+          return containerResult.innerHTML = `<p>Kata yang anda masukan bukan palindrome</p>`;
+        }
       } else {
-        return containerResult.innerHTML = `<p>Kata yang anda masukan bukan palindrome</p>`;
+        return (containerResult.innerHTML = `<p>Huruf harus lebih dari 2</p>`);
       }
     } else {
       return (containerResult.innerHTML = `<p>Kata yang anda masukan harus berupa  <b>String / Huruf</b></p>`);
